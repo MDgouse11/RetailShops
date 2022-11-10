3c15e8f787548b905db69d044ad2328b41b112ef
tmaes <tmaes@tmaes16.Home>
2022-11-10 15:11:24 +0530

    commit proccess

---
+++ Method/src/com/md/Method/Pg.java
@@ -0,0 +1,6 @@
+package com.md.Method;
+
+public interface Pg {
+	public int additionint(int a,int b,int c);
+	public String Addstr(String a,String b);
+}
---
+++ Method/src/com/md/Method/Var.java
@@ -0,0 +1,8 @@
+package com.md.Method;
+
+public class Var {
+public static void main(String args[])
+{
+	
+}
+}
